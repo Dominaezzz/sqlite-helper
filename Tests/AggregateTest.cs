dd687f8f2a0320d05cb47d1d885b270dc7a2ae4c
Dominic Fischer <dominicfischer7@gmail.com>
2017-07-22 23:45:43 +0100

    Improved aggregate unit tests.

--- Tests/AggregateTest.cs
+++ Tests/AggregateTest.cs
@@ -29,96 +29,56 @@ namespace Tests
 		}
 
 
-		[Test(ExpectedResult = 1)]
-		public int TestMin()
+		[Test]
+		public void TestMin()
 		{
-			return _db.DataTable.Min(d => d.Id);
+			int expected = _db.ExecuteScalar<int>("SELECT MIN(Id) FROM DataTable");
+			int actual = _db.DataTable.Min(d => d.Id);
+			Assert.AreEqual(expected, actual);
+		}
+		
+		[Test]
+		public void TestMax()
+		{
+			int expected = _db.ExecuteScalar<int>("SELECT MAX(Id) FROM DataTable");
+			int actual = _db.DataTable.Max(d => d.Id);
+			Assert.AreEqual(expected, actual);
+		}
+		
+		[Test]
+		public void TestSum()
+		{
+			int expected = _db.ExecuteScalar<int>("SELECT SUM(Id) FROM DataTable");
+			int actual = _db.DataTable.Sum(d => d.Id);
+			Assert.AreEqual(expected, actual);
+		}
+		
+		[Test]
+		public void TestAverage()
+		{
+			double expected = _db.ExecuteScalar<double>("SELECT AVG(Id) FROM DataTable");
+			double actual = _db.DataTable.Average(d => d.Id);
+			Assert.AreEqual(expected, actual);
+		}
+		
+		[Test]
+		public void TestCount()
+		{
+			int expected = _db.ExecuteScalar<int>("SELECT COUNT(*) FROM DataTable");
+			int actual = _db.DataTable.Count();
+			Assert.AreEqual(expected, actual);
 		}
 
 		[Test]
 		[Category("Where")]
-		[TestCase(5, 25, ExpectedResult = 5)]
-		[TestCase(45, 65, ExpectedResult = 45)]
-		[TestCase(37, 40, ExpectedResult = 37)]
-		public int TestMinWhere(int min, int max)
+		[TestCase(5, 25)]
+		[TestCase(45, 75)]
+		[TestCase(37, 40)]
+		public void TestCountWhere(int min, int max)
 		{
-			return _db.DataTable
-				.Where(d => d.Id >= min && d.Id <= max)
-				.Min(d => d.Id);
-		}
-
-
-		[Test(ExpectedResult = 100)]
-		public int TestMax()
-		{
-			return _db.DataTable.Max(d => d.Id);
-		}
-
-		[Test]
-		[Category("Where")]
-		[TestCase(5, 25, ExpectedResult = 25)]
-		[TestCase(45, 65, ExpectedResult = 65)]
-		[TestCase(37, 40, ExpectedResult = 40)]
-		public int TestMaxWhere(int min, int max)
-		{
-			return _db.DataTable
-				.Where(d => d.Id >= min && d.Id <= max)
-				.Max(d => d.Id);
-		}
-
-
-		[Test(ExpectedResult = (100 + 1) * 100 / 2)]
-		public int TestSum()
-		{
-			return _db.DataTable.Sum(d => d.Id);
-		}
-
-		[Test]
-		[Category("Where")]
-		[TestCase(5, 25, ExpectedResult = 315)]
-		[TestCase(45, 65, ExpectedResult = 1155)]
-		[TestCase(37, 40, ExpectedResult = 154)]
-		public int TestSumWhere(int min, int max)
-		{
-			return _db.DataTable
-				.Where(d => d.Id >= min && d.Id <= max)
-				.Sum(d => d.Id);
-		}
-
-
-		[Test(ExpectedResult = (100 + 1) / 2.0)]
-		public double TestAverage()
-		{
-			return _db.DataTable.Average(d => d.Id);
-		}
-
-		[Test]
-		[Category("Where")]
-		[TestCase(5, 25, ExpectedResult = 15.0)]
-		[TestCase(45, 65, ExpectedResult = 55.0)]
-		[TestCase(37, 40, ExpectedResult = 38.5)]
-		public double TestAverageWhere(int min, int max)
-		{
-			return _db.DataTable
-				.Where(d => d.Id >= min && d.Id <= max)
-				.Average(d => d.Id);
-		}
-
-
-		[Test(ExpectedResult = 100)]
-		public int TestCount()
-		{
-			return _db.DataTable.Count();
-		}
-
-		[Test]
-		[Category("Where")]
-		[TestCase(5, 25, ExpectedResult = 21)]
-		[TestCase(45, 75, ExpectedResult = 31)]
-		[TestCase(37, 40, ExpectedResult = 4)]
-		public int TestCountWhere(int min, int max)
-		{
-			return _db.DataTable.Count(d => d.Id >= min && d.Id <= max);
+			int expected = _db.ExecuteScalar<int>("SELECT COUNT(Id) FROM DataTable WHERE Id >= ? AND Id <= ?", min, max);
+			int actual = _db.DataTable.Count(d => d.Id >= min && d.Id <= max);
+			Assert.AreEqual(expected, actual);
 		}
 	}
 }
