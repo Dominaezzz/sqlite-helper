f4fde37cd8e100157ac2f6fb1f631ced8d3d127f
Dominic Fischer <dominicfischer7@gmail.com>
2017-07-22 23:33:26 +0100

    Made ColumnProjector much smarter.

--- SQLite.Net/Translation/ColumnProjector.cs
+++ SQLite.Net/Translation/ColumnProjector.cs
@@ -28,73 +28,21 @@ namespace SQLite.Net.Translation
 
 		private readonly HashSet<Expression> _candidates;
 
-		private readonly List<string> _existingAliases;
 		private readonly string _newAlias;
 		private int _columnIndex;
 
-		internal ColumnProjector(Func<Expression, bool> fnCanBeColumn, Expression expression, string newAlias, IEnumerable<string> existingAliases)
+		internal ColumnProjector(Expression expression, string newAlias, IEnumerable<string> existingAliases)
 		{
-			_candidates = Nominator.Nominate(fnCanBeColumn, expression);
-			_existingAliases = existingAliases?.ToList() ?? new List<string>();
+			_candidates = Nominator.Nominate(existingAliases, expression);
 			_newAlias = newAlias;
 		}
-
+		
 		internal static ProjectedColumns ProjectColumns(Expression expression, string newAlias, IEnumerable<string> existingAliases)
 		{
-			return ProjectColumns(CanBeColumn, expression, newAlias, existingAliases);
-		}
-
-		internal static ProjectedColumns ProjectColumns(
-			Func<Expression, bool> fnCanBeColumn, Expression expression, string newAlias, IEnumerable<string> existingAliases)
-		{
-			ColumnProjector projector = new ColumnProjector(fnCanBeColumn, expression, newAlias, existingAliases);
+			ColumnProjector projector = new ColumnProjector(expression, newAlias, existingAliases);
 			return new ProjectedColumns(projector.Visit(expression), projector._columns.AsReadOnly());
 		}
 		
-		/// <summary>
-		/// Determines whether the given expression can be represented as a column in a select expressionss
-		/// </summary>
-		/// <param name="expression"></param>
-		/// <returns></returns>
-		private static bool CanBeColumn(Expression expression)
-		{
-			// by default, push all work in projection to client
-			switch (expression.NodeType)
-			{
-				case ExpressionType.Add:
-				case ExpressionType.AddChecked:
-				case ExpressionType.Subtract:
-				case ExpressionType.SubtractChecked:
-				case ExpressionType.Multiply:
-				case ExpressionType.MultiplyChecked:
-				case ExpressionType.Divide:
-				case ExpressionType.Modulo:
-				case ExpressionType.And:
-				case ExpressionType.AndAlso:
-				case ExpressionType.Or:
-				case ExpressionType.OrElse:
-					return true;
-				default:
-					return MustBeColumn(expression);
-			}
-		}
-
-		private static bool MustBeColumn(Expression expression)
-		{
-			switch (expression.NodeType)
-			{
-				case (ExpressionType)DbExpressionType.Column:
-				case (ExpressionType)DbExpressionType.Scalar:
-				case (ExpressionType)DbExpressionType.Aggregate:
-				case (ExpressionType)DbExpressionType.Function:
-				case (ExpressionType)DbExpressionType.TypeChange:
-				case (ExpressionType)DbExpressionType.HostParameter:
-					return true;
-				default:
-					return false;
-			}
-		}
-
 		public override Expression Visit(Expression expression)
 		{
 			if (_candidates.Contains(expression))
@@ -105,10 +53,6 @@ namespace SQLite.Net.Translation
 					ColumnExpression mapped;
 					// If column has been mapped already just return it.
 					if (_map.TryGetValue(column, out mapped)) return mapped;
-
-					// If the column does not belong to an immediately existing alias
-					// then it must be referring to outer scope.
-					if (!_existingAliases.Contains(column.Alias)) return column;
 					
 					string columnName = GetUniqueColumnName(column.Name);
 
@@ -156,20 +100,110 @@ namespace SQLite.Net.Translation
 		/// </summary>
 		private class Nominator : DbExpressionVisitor
 		{
-			private readonly Func<Expression, bool> _fnCanBeColumn;
+			private readonly List<string> _existingAliases;
 			private readonly HashSet<Expression> _candidates = new HashSet<Expression>();
 			private bool _isBlocked = false;
 
-			private Nominator(Func<Expression, bool> fnCanBeColumn)
+			private Nominator(IEnumerable<string> existingAliases)
 			{
-				_fnCanBeColumn = fnCanBeColumn;
+				_existingAliases = existingAliases?.ToList() ?? new List<string>();
 			}
 
-			internal static HashSet<Expression> Nominate(Func<Expression, bool> fnCanBeColumn, Expression expression)
+			internal static HashSet<Expression> Nominate(IEnumerable<string> existingAliases, Expression expression)
 			{
-				Nominator nominator = new Nominator(fnCanBeColumn);
+				Nominator nominator = new Nominator(existingAliases);
 				nominator.Visit(expression);
 				return nominator._candidates;
+			}
+
+			/// <summary>
+			/// Determines whether the given expression can be represented as a column in a select expressionss
+			/// </summary>
+			/// <param name="expression"></param>
+			/// <returns></returns>
+			private bool CanBeColumn(Expression expression)
+			{
+				switch (expression.NodeType)
+				{
+					case ExpressionType.Add:
+					case ExpressionType.AddChecked:
+					case ExpressionType.Subtract:
+					case ExpressionType.SubtractChecked:
+					case ExpressionType.Multiply:
+					case ExpressionType.MultiplyChecked:
+					case ExpressionType.Divide:
+					case ExpressionType.Modulo:
+					case ExpressionType.And:
+					case ExpressionType.AndAlso:
+					case ExpressionType.Or:
+					case ExpressionType.OrElse:
+					case ExpressionType.ExclusiveOr:
+					case ExpressionType.OnesComplement:
+					case ExpressionType.Not:
+					case ExpressionType.Negate:
+					case ExpressionType.NegateChecked:
+					case ExpressionType.UnaryPlus:
+					case ExpressionType.RightShift:
+					case ExpressionType.LeftShift:
+					case ExpressionType.Equal:
+					case ExpressionType.NotEqual:
+					case ExpressionType.GreaterThan:
+					case ExpressionType.GreaterThanOrEqual:
+					case ExpressionType.LessThan:
+					case ExpressionType.LessThanOrEqual:
+					case ExpressionType.Coalesce:
+						return true;
+					case ExpressionType.Constant:
+						return true;
+					case ExpressionType.Convert:
+					case ExpressionType.ConvertChecked:
+						return true;
+					case ExpressionType.Conditional:
+						return true;
+					case ExpressionType.Call:
+						Type declaringType = ((MethodCallExpression) expression).Method.DeclaringType;
+						if (declaringType == typeof(DateTime) || declaringType == typeof(DateTimeOffset) ||
+							declaringType == typeof(TimeSpan) || declaringType == typeof(string) ||
+							declaringType == typeof(decimal)  || declaringType == typeof(Math))
+						{
+							return true;
+						}
+						goto default;
+					case ExpressionType.MemberAccess:
+						Type mDeclaringType = ((MemberExpression) expression).Member.DeclaringType;
+						if (mDeclaringType == typeof(DateTime) || mDeclaringType == typeof(DateTimeOffset) ||
+							mDeclaringType == typeof(TimeSpan) || mDeclaringType == typeof(string))
+						{
+							return true;
+						}
+						goto default;
+					default:
+						return MustBeColumn(expression);
+				}
+			}
+
+			private bool MustBeColumn(Expression expression)
+			{
+				switch (expression.NodeType)
+				{
+					case (ExpressionType)DbExpressionType.Column:
+						ColumnExpression column = (ColumnExpression) expression;
+						// If the column does not belong to an immediately existing alias
+						// then it must be referring to outer scope.
+						if (_existingAliases.Contains(column.Alias))
+						{
+							return true;
+						}
+						goto default;
+					case (ExpressionType)DbExpressionType.Scalar:
+					case (ExpressionType)DbExpressionType.Aggregate:
+					case (ExpressionType)DbExpressionType.Function:
+					case (ExpressionType)DbExpressionType.TypeChange:
+					case (ExpressionType)DbExpressionType.HostParameter:
+						return true;
+					default:
+						return false;
+				}
 			}
 
 			public override Expression Visit(Expression expression)
@@ -177,28 +211,28 @@ namespace SQLite.Net.Translation
 				if (expression == null) return null;
 
 				bool saveIsBlocked = _isBlocked;
-				_isBlocked = false;
 				if (MustBeColumn(expression))
 				{
 					_candidates.Add(expression);
-					// don't merge saveIsBlocked
+					_isBlocked = false;
 				}
 				else
 				{
 					base.Visit(expression);
 					if (!_isBlocked)
 					{
-						if (_fnCanBeColumn(expression))
+						if (CanBeColumn(expression))
 						{
 							_candidates.Add(expression);
+							_isBlocked = false;
 						}
 						else
 						{
 							_isBlocked = true;
 						}
 					}
-					_isBlocked |= saveIsBlocked;
 				}
+				_isBlocked |= saveIsBlocked;
 				return expression;
 			}
 		}
