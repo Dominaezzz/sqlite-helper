7c98dda86e43fad1694f30af804a251e2851da60
Dominic Fischer <dominicfischer7@gmail.com>
2017-07-28 22:18:40 +0100

    Added a sub-query test.

---
+++ Tests/SubQueryTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture]
+    public class SubQueryTest
+	{
+		private ChinookDatabase _db;
+
+		[OneTimeSetUp]
+		public void TestSetUp()
+		{
+			_db = new ChinookDatabase();
+			_db.Logger = Console.Out;
+		}
+
+		[OneTimeTearDown]
+		public void TestTearDown()
+		{
+			_db.Dispose();
+		}
+
+
+		[Test]
+		public void TestSimpleSubQuery()
+		{
+			const string querySQL =
+				"SELECT p.Name, (SELECT COUNT(*) FROM PlaylistTrack WHERE PlaylistId == p.PlaylistId) " +
+				"FROM Playlist p";
+			using (var query = _db.ExecuteQuery(querySQL))
+			{
+				var result = _db.Playlists.Select(p => new
+				{
+					Playlist = p.Name,
+					TrackCount = _db.PlaylistTracks.Count(pt => pt.PlaylistId == p.PlaylistId)
+				});
+				foreach (var item in result)
+				{
+					Assert.IsTrue(query.Step());
+					Assert.AreEqual(query.GetText(0), item.Playlist);
+					Assert.AreEqual(query.GetInt(1), item.TrackCount);
+				}
+				Assert.IsFalse(query.Step());
+			}
+		}
+	}
+}
